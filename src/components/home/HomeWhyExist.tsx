4d71e43b002f4af87d37769475ece46eef4a3879
Goodness Adewuyi <goodnessadewuyi8@gmail.com>
2025-08-26 15:04:18 +0000

    Made changes

--- src/components/home/HomeWhyExist.tsx
+++ src/components/home/HomeWhyExist.tsx
@@ -28,7 +28,7 @@ const HomeWhyExist = () => {
               <Image
                 width={800}
                 height={600}
-                src="/images/home-page/why-exist.jpg"
+                src="/images/home-page/why-section/home-why.jpg"
                 alt="Calm trading vs noisy trading platforms comparison"
                 className="w-full h-screen object-cover transition-transform duration-[0.6s] min-h-[clamp(40rem,50vh,60rem)] lg:min-h-[clamp(35rem,45vh,50rem)] md:min-h-[clamp(30rem,40vh,45rem)] saturate-[0.8] contrast-[1.1] brightness-[0.95] group-hover:scale-[1.02]"
               />
